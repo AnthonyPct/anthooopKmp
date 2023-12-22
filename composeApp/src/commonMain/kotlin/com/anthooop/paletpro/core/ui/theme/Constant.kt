ffb12327b119783af0dd527c37b4d6df9200d6ea
Anthony Picquet <anthony.picquet@checkandvisit.com>
2023-12-22 18:16:19 +0100

    First commit

---
+++ composeApp/src/commonMain/kotlin/com/anthooop/paletpro/core/ui/theme/Constant.kt
@@ -0,0 +1,13 @@
+package com.anthooop.paletpro.core.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+val CheckboxSize = 24.dp
+val ButtonIconSize = 16.dp
+val LoaderSize = 80.dp
+val LoaderLineSize = 3.dp
+val PrimaryButtonHeight = 48.dp
+val BottomSheetHeight = 220.dp
+val PrimaryMediumButtonHeight = 36.dp
+val PrimarySmallButtonHeight = 28.dp
+val ProjectDotSize = 12.dp
---
+++ composeApp/src/commonMain/kotlin/com/anthooop/paletpro/core/ui/theme/Spacing.kt
@@ -0,0 +1,13 @@
+
+
+class Spacing {
+    companion object {
+        const val small = 4
+        const val medium = 8
+        const val large = 12
+        const val xLarge = 16
+        const val giant = 20
+        const val xGiant = 24
+        const val xxGiant = 42
+    }
+}
